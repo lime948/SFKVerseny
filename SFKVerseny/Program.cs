fad4fa9ca50ec0963288c926e05431362c6106c1
lime948 <oreskoz@kkszki.hu>
2025-12-10 20:46:06 +0100

    alapok

--- SFKVerseny/Program.cs
+++ SFKVerseny/Program.cs
@@ -12,13 +12,44 @@ namespace SFKVerseny
     {
         static void Main(string[] args)
         {
+            List<TableRowData> versenyzok = new List<TableRowData>();
+            Console.Write("Adja meg a versenyző nevét: ");
+            string nev = Console.ReadLine();
+            Console.Write("Adja meg az 1. lengetésen elért pontszámot: ");
+            int pont1 = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg az 2. lengetésen elért pontszámot: ");
+            int pont2 = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg az 3. lengetésen elért pontszámot: ");
+            int pont3 = int.Parse(Console.ReadLine());
+            int maxPont = Math.Max(pont1, Math.Max(pont2, pont3));
+
+            versenyzok.Add(new TableRowData {Name = nev, Pont1 = pont1, Pont2 = pont2, Pont3 = pont3, Maxpont = maxPont});
+            var sorted = versenyzok.OrderByDescending(r => r.Maxpont).ToList();
+
             var doc = new HtmlDocument();
             doc.Load("SFKVerseny.html");
-            var table = doc.DocumentNode.SelectSingleNode("//table[@id='myTable']");
-            var newRow = HtmlNode.CreateNode($"<tr><td></td><td>Anna</td><td>3</td><td>Anna</td><td>3</td><td>Anna</td></tr>");
-            table.AppendChild(newRow);
-            doc.Save("table.html");
+            var table = doc.DocumentNode.SelectSingleNode("//table[@id='adatok']");
+            table.RemoveAllChildren();
+            var headerRow = HtmlNode.CreateNode(
+                "<tr><th>Név</th><th>1. lengetés</th><th>2. lengetés</th><th>3. lengetés</th><th>Legjobb pontszám</th><th>Összesített pontszám</th></tr>"
+                );
+            foreach (var r in sorted)
+            {
+                var newRow = HtmlNode.CreateNode(
+                        $"<tr><td>{nev}</td><td>{pont1}</td><td>{pont2}</td><td>{pont3}</td><td>{maxPont}</td><td>{r.Maxpont}</td></tr>"
+                        );
+                table.AppendChild(newRow);
+            }
+            doc.Save("SFKVerseny.html");
 
+        }
+        public class TableRowData
+        {
+            public string Name { get; set; }
+            public int Pont1 { get; set; }
+            public int Pont2 { get; set; }
+            public int Pont3 { get; set; }
+            public int Maxpont { get; set; }
         }
 
     }
