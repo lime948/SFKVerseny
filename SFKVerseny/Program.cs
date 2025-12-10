268524f42d3eb9022dd4ffcf2d19aa8633e1e21a
lime948 <oreskoz@kkszki.hu>
2025-12-10 19:50:06 +0100

    Add project files.

---
+++ SFKVerseny/Program.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFKVerseny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new HtmlDocument();
+            doc.Load("SFKVerseny.html");
+            var table = doc.DocumentNode.SelectSingleNode("//table[@id='myTable']");
+            var newRow = HtmlNode.CreateNode($"<tr><td></td><td>Anna</td><td>3</td><td>Anna</td><td>3</td><td>Anna</td></tr>");
+            table.AppendChild(newRow);
+            doc.Save("table.html");
+
+        }
+
+    }
+}
